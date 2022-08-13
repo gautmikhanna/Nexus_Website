b4c87c5ee6da72b127903ca098d49e719be4c126
Anvesh Mishra <anveshgreat11@gmail.com>
2022-08-13 14:01:59 +0530

    Revert "enhancement : added root variables and link for primary font (#17)" (#18)
    This reverts commit 781f68b1175673e148e13c66c55279bf18f6682f.

--- build/script.js
+++ build/script.js
@@ -1,26 +1,34 @@
 const pages = ["landing", "about", "sponsors"];
 const load = async (page, isReload = false, direction = "left") => {
-  document.querySelectorAll(".toggle-btn").forEach((btn) => btn.disabled = true);
+  document
+    .querySelectorAll(".toggle-btn")
+    .forEach((btn) => (btn.disabled = true));
   var mid_data = "";
   var left_data = "";
   var right_data = "";
   const previous_slide = page ? page - 1 : pages.length - 1;
   const next_slide = (page + 1) % pages.length;
-  fetch("./content/" + pages[page] + ".html").then((response) => response.text()).then((data) => {
-    mid_data = data;
-  });
-  fetch("./content/" + pages[next_slide] + ".html").then((response) => response.text()).then((data) => {
-    right_data = data;
-  });
-  fetch("./content/" + pages[previous_slide] + ".html").then((response) => response.text()).then((data) => {
-    left_data = data;
-    if (isReload) {
-      document.querySelector(".loader").classList.add("fadeOut");
-      setTimeout(() => {
-        document.querySelector(".loader").classList.add("no-display");
-      }, 1e3);
-    }
-  });
+  fetch("./content/" + pages[page] + ".html")
+    .then((response) => response.text())
+    .then((data) => {
+      mid_data = data;
+    });
+  fetch("./content/" + pages[next_slide] + ".html")
+    .then((response) => response.text())
+    .then((data) => {
+      right_data = data;
+    });
+  fetch("./content/" + pages[previous_slide] + ".html")
+    .then((response) => response.text())
+    .then((data) => {
+      left_data = data;
+      if (isReload) {
+        document.querySelector(".loader").classList.add("fadeOut");
+        setTimeout(() => {
+          document.querySelector(".loader").classList.add("no-display");
+        }, 1e3);
+      }
+    });
   if (direction === "left") {
     document.getElementById("main-content").classList.add("slide-left");
     document.querySelector(".right-slide").classList.add("slide-left");
@@ -44,7 +52,9 @@ const load = async (page, isReload = false, direction = "left") => {
     document.getElementById("main-content").innerHTML = mid_data;
     document.querySelector(".right-slide").innerHTML = right_data;
     document.querySelector(".left-slide").innerHTML = left_data;
-    document.querySelectorAll(".toggle-btn").forEach((btn) => btn.disabled = false);
+    document
+      .querySelectorAll(".toggle-btn")
+      .forEach((btn) => (btn.disabled = false));
   }, 1e3);
 };
 const next = (event) => {
@@ -53,10 +63,8 @@ const next = (event) => {
   load(page_serve);
 };
 const previous = (event) => {
-  if (!page_serve)
-    page_serve = pages.length - 1;
-  else
-    page_serve -= 1;
+  if (!page_serve) page_serve = pages.length - 1;
+  else page_serve -= 1;
   page_serve = page_serve % pages.length;
   console.log(page_serve);
   load(page_serve, false, "right");
