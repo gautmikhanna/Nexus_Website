c093476b635b98d78947cc149391ec264bdfaf21
Prerna Sharma <89515816+Prerna-0202@users.noreply.github.com>
2022-08-13 19:16:52 +0530

    Feat: Landing Page responsive (#20)
    * Feat: Landing Page responsive
    
    * Update styles.css
    
    * Fixed Errors

--- scripts/index.js
+++ scripts/index.js
@@ -101,8 +101,3 @@ load(page_serve, true);
 // page toggle
 document.getElementById("next").addEventListener("click", next);
 document.getElementById("previous").addEventListener("click", previous);
-
-// page animation
-document.querySelector(".fa-angle-down").addEventListener("click", () => {
-  window.scrollBy(0, 1000);
-});
