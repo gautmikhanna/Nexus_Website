d26eaa19fd4bfdeb8b522418e5ccba4d650aad14
Pulkit Gupta <pulkit30.bsr@gmail.com>
2022-08-25 10:52:19 +0530

    chore: production build (#47)
    * chore: production build
    
    * remove: sponsors and schedule section
    
    * fix: readme.md

---
+++ builder.config.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+const path = require("path");
+const dirs = ["/scripts", "/stylesheets"];
+const build_dirs = ["/builds/script.js", "/builds/style.css"];
+
+build_dirs.forEach((dir) => {
+  fs.writeFileSync(path.join(__dirname, dir), "");
+  console.log("cleaning up...");
+});
+
+dirs.forEach((dir, index) => {
+  let data = fs.readFileSync(path.join(__dirname, dir + "/main"));
+  data = data.toString().split("/");
+  data.forEach((file) => {
+    let file_data = fs.readFileSync(path.join(__dirname, dir + "/" + file));
+    fs.appendFileSync(
+      path.join(__dirname, build_dirs[index]),
+      file_data.toString()
+    );
+  });
+  console.log("build success 🎉");
+});
--- readme.md
+++ readme.md
@@ -2,8 +2,6 @@
 
 <img src="https://user-images.githubusercontent.com/76155456/184479063-bf2827fb-3c48-4f10-8692-e763e01dff1d.svg" width="300px"/>
 
-
-
 [![Netlify Status](https://api.netlify.com/api/v1/badges/8269b73b-ad77-425e-b027-76299676e5c7/deploy-status)](https://app.netlify.com/sites/hacknexus/deploys)
 
 </div>
@@ -12,7 +10,8 @@
 
 1. `git clone https://github.com/ElixirTechCommunity/Nexus_Website.git`
 2. `open project in vscode.`
-3. `create your personal branch.` 
+3. `create your personal branch.`
+4. open terminal and run `npm install` (optional)
 
 ### For development
 
