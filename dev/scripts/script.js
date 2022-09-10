fe65e48a4fbca61f5b2e2fcd13307cc349cbbe65
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2022-09-10 17:04:05 +0530

    chore: decouple files

--- dev/scripts/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("scroll", (e) => {
-  if (window.pageYOffset > 778) {
-    document.getElementById("navbar").classList.add("nav-bg");
-  } else {
-    document.getElementById("navbar").classList.remove("nav-bg");
-  }
-});
